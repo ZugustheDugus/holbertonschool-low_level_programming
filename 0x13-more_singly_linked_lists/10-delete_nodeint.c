d822bd8c038ce317b9b0f9ee219fbbfe4ba062a5
ZugustheDugus <h1cooper2019@protonmail.com>
2021-11-11 21:06:24 -0800

    adding task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,6 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 listint_t *next, *current, *temp = *head;
 unsigned int i;
+index = 0;
 
 if (*head == NULL || head == NULL)
 {
