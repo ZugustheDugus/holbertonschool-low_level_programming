577ce914d57e560b17e6cfd682ff6ea319de6b82
ZugustheDugus <h1cooper2019@protonmail.com>
2021-11-12 17:37:00 -0800

    adding task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets a bit at an index to 0
+ * @n: binary number to alter
+ * @index: the index at which to alter n
+ * Return: -1 for error, 1 for success
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+if (*n == '\0' || index > 31)
+return (-1);
+
+*n = ((0 << index) | *n);
+return (1);
+}
