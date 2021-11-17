ffd1b8ec8d6ae3cf5715409a8b6b0b7dad522810
ZugustheDugus <h1cooper2019@protonmail.com>
2021-11-17 13:51:43 -0800

    adding task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file
+ * @filename: name of tile to read
+ * @letters: number of letters in the filename
+ * Return: Returns 0 if there is nothing to read
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int fd, len, chars;
+char *buffer;
+
+if (filename == NULL || letters == 0)
+return (0);
+
+fd = open (filename, O_RDONLY);
+
+if (fd == -1)
+return (0);
+
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+return (0);
+
+len = read(fd, buffer, letters);
+
+chars = write(STDOUT_FILENO, buffer, len);
+
+if (len == -1 || chars == -1)
+return (0);
+
+free(buffer);
+close(fd);
+return (chars);
+}
