808a522d3dbcc4073a90c0b1e891b46e89624b78
ZugustheDugus <h1cooper2019@protonmail.com>
2021-11-17 15:29:07 -0800

    please stop breaking

--- 0x15-file_io/_strlen.c
+++ 0x15-file_io/_strlen.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _strlen - Entry point
- *
- * @s: Point to the string
- * Return: length of string
+ * _strlen - Gets the length of a string
+ * @s: Pointer to the string
+ * Return: the string's length
  */
 
 int _strlen(char *s)
 {
 int i = 0;
 
-while (s[i] != '\0')
+while (*s != '\0')
 {
+s++;
 i++;
 }
 return (i);
