a01a29cb64b319411308302c2c0693bd13b89893
ZugustheDugus <h1cooper2019@protonmail.com>
2021-12-12 21:28:30 -0800

    trying to find out what weird check is by changing things

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,15 +9,25 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table_meme = malloc(sizeof(hash_table_t));
+	unsigned long int i = 0;
 
-	if (size == 0 || table_meme == NULL)
+	if (!table_meme)
+	{
+		free(table_meme);
 		return (NULL);
+	}
 
 	table_meme->size = size;
-	table_meme->array = calloc(table_meme->size, sizeof(hash_table_t *));
+	table_meme->array = malloc(sizeof(hash_node_t *) * size);
 
-	if (table_meme->array == NULL)
+	if (!table_meme->array)
+	{
+		free(table_meme->array[i]);
+		free(table_meme);
 		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		table_meme->array[i] = NULL;
 
 	return (table_meme);
 }
