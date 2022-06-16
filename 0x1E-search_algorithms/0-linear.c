6bc3e578375e984802fbb902c6b571ea48b90f6e
ZugustheDugus <h1cooper2019@pronmail.com>
2022-06-16 15:16:23 -0700

    moving back out of sandbox

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,10 +15,11 @@ int linear_search(int *array, size_t size, int value)
   if (array == NULL)
     return (-1);
 
-  for (i = 0; i < size; i++) {
+  for (i = 0; i < size; i++)
+    {
     printf("Value checked array[%i] = [%i]\n", (int)i, array[i]);
     if (array[i] == value)
-      return i;
+      return (i);
   }
-  return -1;
+  return (-1);
 }
