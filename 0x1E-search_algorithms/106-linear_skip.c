5d605f27325aa26a69ada682c887eb862be34413
ZugustheDugus <h1cooper2019@protonmail.com>
2022-06-30 18:42:18 -0500

    Task

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,38 @@
+#include "search_algos.h"
+
+/**
+ * print_array - Prints an array of ints
+ *
+ * @array: The array
+ * @size: Number of elements in array
+ */
+void print_array(const int *array, size_t size)
+{
+size_t i;
+
+i = 0;
+while (array && i < size)
+{
+if (i > 0)
+printf(", ");
+printf("%d", array[i]);
+i++;
+}
+printf("\n");
+}
+
+/**
+* linear_skip - Searches for value in sorted array
+* @list: Pointer to first element of list
+* @value: Value to search for
+* Return: Index of value or -1 (NULL)
+**/
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+if (!value || !list)
+return (NULL);
+
+printf("Value checked at index [%d] = [%d]\n", value, value);
+return (list);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,13 +1,31 @@
 #ifndef SEARCH_ALGOS
 #define SEARCH_ALGOS
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int linear_search(int *array, size_t size, int value);
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
 int binary_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value);
+void print_array(const int *array, size_t size);
 int advanced_binary(int *array, size_t size, int value);
-int recursive_binary(int *array, int value, size_t left, size_t right,
-int *val);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif
