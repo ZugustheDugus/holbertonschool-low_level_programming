24e5d9be45923f743404b31eb6809a41c0fa2c46
ZugustheDugus <h1cooper2019@protonmail.com>
2022-06-30 18:47:34 -0500

    asdf

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,38 +1,50 @@
 #include "search_algos.h"
 
 /**
- * print_array - Prints an array of ints
- *
- * @array: The array
- * @size: Number of elements in array
+ * linear_skip - linear search in a skip list
+ * @list: pointer to linked list being searched
+ * @value: value to search for
+ * Return: found index or NULL if not found
  */
-void print_array(const int *array, size_t size)
-{
-size_t i;
-
-i = 0;
-while (array && i < size)
-{
-if (i > 0)
-printf(", ");
-printf("%d", array[i]);
-i++;
-}
-printf("\n");
-}
-
-/**
-* linear_skip - Searches for value in sorted array
-* @list: Pointer to first element of list
-* @value: Value to search for
-* Return: Index of value or -1 (NULL)
-**/
 
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-if (!value || !list)
-return (NULL);
+	skiplist_t *temp = NULL;
 
-printf("Value checked at index [%d] = [%d]\n", value, value);
-return (list);
+	if (list)
+	{
+		temp = list;
+		while (temp->express)
+		{
+			printf("Value checked at index [%lu] = [%i]\n",
+					temp->express->index, temp->express->n);
+			if (temp->express->n >= value)
+			{
+				printf("Value found between indexes [%lu] and [%lu]\n",
+						temp->index, temp->express->index);
+				break;
+			}
+			temp = temp->express;
+		}
+		if (!temp->express)
+		{
+			list = temp;
+			while (list->next)
+				list = list->next;
+			printf("Value found between indexes [%lu] and [%lu]\n",
+					temp->index, list->index);
+		}
+		list = temp;
+		while (list != temp->express)
+		{
+			printf("Value checked at index [%lu] = [%d]\n",
+			list->index, list->n);
+			if (list->n == value)
+				break;
+			list = list->next;
+		}
+		if (list != temp->express)
+			return (list);
+	}
+	return (NULL);
 }
